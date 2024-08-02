32cb8b1687becb6fd73fd7cb9f3f3618995b51c1
Abdulxaliq <Agaye_ck17@itstep.edu.az>
2024-08-02 12:20:24 +0400

    Add project files.

---
+++ homework10/homework10.cpp
@@ -0,0 +1,162 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //for (int i = 1; i < 11; i++)
+    //{
+    //	for (int j = 1; j < 11; j++)
+    //	{
+    //			cout << '*';
+    //	}
+    //	cout << endl;
+    //}
+
+
+
+
+
+
+    //for (int i = 0; i < 10; ++i)
+    //{
+    //    cout << '*';
+    //}
+    //cout << endl;
+
+    //for (int i = 0; i < 10 - 2; ++i)
+    //{
+    //    cout << '*';
+
+    //    for (int j = 0; j < 10 - 2; ++j) 
+    //    {
+    //       cout << ' ';
+    //    }
+    //    cout << '*' << endl;
+    //}
+
+    //for (int i = 0; i < 10; ++i) 
+    //{
+    //    cout << '*';
+    //}
+    //cout << endl;
+
+
+
+    //for (int i = 0; i < 10; ++i) {
+    //    cout << '*';
+    //}
+    //cout << endl;
+    //for (int i = 0; i < 10 - 2; ++i) {
+    //    cout << '*';
+    //    for (int j = 0; j < 20 - 2; ++j) {
+    //        cout << ' ';
+    //    }
+    //    cout << '*';
+
+    //    for (int j = 0; j < 8; ++j) {
+    //        cout << ' ';
+    //    }
+    //    cout << '*';
+
+    //    for (int j = 0; j < 10 - 2; ++j) {
+    //        cout << ' ';
+    //    }
+    //    cout << '*' << endl;
+    //}
+
+    //for (int i = 0; i < 20; ++i) {
+    //    cout << '*';
+    //}
+    //cout << endl;
+
+    //for (int i = 0; i < 8; ++i) {
+    //    cout << '*';
+    //    for (int j = 0; j < 10 - 2; ++j) {
+    //        cout << ' ';
+    //    }
+    //    cout << '*';
+
+
+    //    for (int j = 0; j < 8; ++j) {
+    //        cout << ' ';
+    //    }
+    //    cout << '*' << endl;
+    //}
+
+    //for (int i = 0; i < 10 + 8 + 2; ++i) {
+    //    cout << '*';
+    //}
+    //cout << endl;
+
+
+
+    //for (int i = 0; i < 5; ++i) {
+    //    for (int j = 0; j < 21; ++j) {
+    //        if (i == 0 || i ==5 - 1) {
+    //            cout << '*';
+    //        }
+    //        else if (j == 0 || j == 21 - 1) {
+    //            cout << '*';
+    //        }
+    //        else if (j % (4 + 5) < 4 || j % (4 +5) >= 21 -4) {
+    //            cout << '*';
+    //        }
+    //        else {
+    //            cout << ' ';
+    //        }
+    //    }
+    //    cout << endl;
+    //}
+
+    //for (int i = 0; i <21; ++i) {
+    //    cout << ' ';
+    //}
+    //cout << endl;
+
+    //for (int i = 0; i < 8; ++i) { 
+    //    cout << '*'; 
+    //    for (int j = 1; j < 21 - 1; ++j) {
+    //        cout << ' ';
+    //    }
+    //    cout << '*' << endl;
+    //}
+
+    //for (int i = 0; i < 5; ++i) {
+    //    for (int j = 0; j < 21; ++j) {
+    //        if (i == 0 || i ==5 - 1) {
+    //            cout << '*'; 
+    //        }
+    //        else if (j == 0 || j == 21 - 1) {
+    //            cout << '*'; 
+    //        }
+    //        else if (j % (4 + 5) < 4 || j % (4 +5) >= 21 - 4) {
+    //            cout << '*'; 
+    //        }
+    //        else {
+    //            cout << ' ';
+    //        }
+    //    }
+    //    cout << endl;
+    //}
+
+    //return 0;
+
+
+    //int size = 11;
+    //for (int i = 0; i < size; ++i) {
+    //    for (int j = 0; j < size; ++j) {
+    //        if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
+    //            cout << '*';
+    //        }
+    //        else if (i == j || j == size - i - 1) {
+    //            cout << '*';
+    //        }
+    //        else {
+    //            cout << ' ';
+    //        }
+    //    }
+    //    cout << endl;
+    //}
+}
+
